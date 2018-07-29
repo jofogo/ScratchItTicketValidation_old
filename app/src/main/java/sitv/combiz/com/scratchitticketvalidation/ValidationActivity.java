40e77e358a73898c9530fe9ae5f056ff9cba961d
John Forrest Go <go_jfd@yahoo.com>
2018-07-29 09:44:55 +0800

    Major functionality: - Scanner feature now added and tested working for PDF417 and QR codes - Added inline documentation to ValidationActivity

--- app/src/main/java/sitv/combiz/com/scratchitticketvalidation/ValidationActivity.java
+++ app/src/main/java/sitv/combiz/com/scratchitticketvalidation/ValidationActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,19 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.ResultPoint;
+import com.google.zxing.client.android.BeepManager;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.BarcodeView;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class ValidationActivity extends AppCompatActivity {
     ImageButton btnAdd;
@@ -27,11 +40,39 @@ public class ValidationActivity extends AppCompatActivity {
     ImageButton btnSettings;
     ImageButton btnLogout;
     Boolean menuHidden = true;
+    private DecoratedBarcodeView tViewScanner;
+    private BeepManager beepManager;
+    private String lastTicketCode="";
+    private DefaultDecoderFactory defaultDecoderFactory;
+
 
 
     ArrayList<String> ticketCodes = new ArrayList<String>();
-    private final int ticketCodeLength = 10;
+    private final int ticketCodeLength = 22;
 
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText().equals(null) || result.getText().equals(lastTicketCode)) {
+                return;
+            }
+            lastTicketCode = result.getText();
+            Log.i("SCANRESULT", "" + lastTicketCode);
+            addTicket(lastTicketCode);
+            //tViewScanner.setStatusText(lastTicketCode);
+            //txtTicketCode.setText(lastTicketCode);
+            beepManager.playBeepSoundAndVibrate();
+
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+
+        }
+    };
+
+    //Menu pressed handler
     public void menuPressed(View view) {
         if (menuHidden) {
             menuShow();
@@ -43,6 +84,7 @@ public class ValidationActivity extends AppCompatActivity {
 
     }
 
+    //Shows the top menu
     private void menuShow() {
         int translationYBy = 100;
         int duration = 100;
@@ -55,6 +97,7 @@ public class ValidationActivity extends AppCompatActivity {
 
     }
 
+    //Hides the top menu
     private void menuHide(){
         int translationYBy = -100;
         btnAbout.setVisibility(View.INVISIBLE);
@@ -66,25 +109,39 @@ public class ValidationActivity extends AppCompatActivity {
     }
 
 
-
+    //Add button pressed (manually add a ticketcode):
+    //1. Check if null text is being added
+    //2. call addTicket if Ticket Code text field is not null
     public void ticketAdd(View view) {
         String ticketCode = txtTicketCode.getText().toString();
         if (ticketCode.equals("")) {
             Toast.makeText(this, "Ticket code is empty!", Toast.LENGTH_SHORT).show();
-        } else if (ticketCode.length() < ticketCodeLength) {
-            Toast.makeText(this, "Ticket code should be at least " +ticketCodeLength+ " characters!", Toast.LENGTH_SHORT).show();
         } else {
-            if (ticketCodes.contains(ticketCode)) {
-                Toast.makeText(this, "Ticket code was already used!", Toast.LENGTH_SHORT).show();
-            } else {
-                ticketCodes.add(ticketCode);
-                Toast.makeText(this, "Ticket code: " + ticketCode + " was added.", Toast.LENGTH_SHORT).show();
-                txtTicketCount.setText("" + ticketCodes.size());
-                txtTicketCode.setText("");
-            }
+            addTicket(ticketCode);
         }
     }
 
+
+    //Add a ticket:
+    //1. Allows to manually add a Ticket Code if the Ticket Code text-field is populated;
+    // - Ticket code should not be a duplicate
+    //2. Automatically adds a ticket code if added using the scanner
+    private void addTicket(String ticketCode) {
+        if (ticketCode.length() < ticketCodeLength) {
+            Toast.makeText(this, "Ticket code should be at least " +ticketCodeLength+ " characters!", Toast.LENGTH_SHORT).show();
+        } else if (ticketCodes.contains(ticketCode)) {
+            Toast.makeText(this, "Ticket code was already used!", Toast.LENGTH_SHORT).show();
+        } else {
+            ticketCodes.add(ticketCode);
+            Toast.makeText(this, "Ticket code: " + ticketCode + " was added.", Toast.LENGTH_SHORT).show();
+            txtTicketCount.setText("" + ticketCodes.size());
+            txtTicketCode.setText("");
+        }
+
+    }
+
+
+    //Remove all saved Ticket Codes (clears the array)
     public void ticketRemoveAll(View view) {
         new AlertDialog.Builder(this)
                 .setTitle("Remove all tickets")
@@ -96,7 +153,7 @@ public class ValidationActivity extends AppCompatActivity {
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 ticketCodes.clear();
                                 txtTicketCount.setText("0");
-
+                                lastTicketCode = "";
                             }
                         }
 
@@ -118,12 +175,16 @@ public class ValidationActivity extends AppCompatActivity {
 
     }
 
+    //Logout sequence - Moves the Activity back to the login (MainActivity)
     private void logout() {
         Intent intent = new Intent(ValidationActivity.super.getBaseContext(), MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
+    //User logout:
+    // 1. Logs out automatically if Ticket Count = 0
+    // 2. Logs out after pop-up confirmation if Ticket Count > 0
     public void userLogout(View view) {
         if (txtTicketCount.getText().toString().equals("0")) {
             logout();
@@ -153,34 +214,72 @@ public class ValidationActivity extends AppCompatActivity {
 
     }
 
-
+    //Initiate logout sequence when hardware Back button is pressed
     @Override
     public void onBackPressed() {
         userLogout(null);
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        tViewScanner.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        tViewScanner.pause();
+    }
+
+    public void pause (View view) {
+        tViewScanner.pause();
+    }
+
+    public void resume (View view) {
+        tViewScanner.resume();
+    }
+
+    public void triggerScan(View view) {
+        tViewScanner.decodeSingle(callback);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validation);
+
+        //ValidationActivity main objects
         btnAdd = (ImageButton) findViewById(R.id.btnAdd);
         btnUpload = (ImageButton) findViewById(R.id.btnUpload);
         btnRemove = (ImageButton) findViewById(R.id.btnRemove);
         txtTicketCount = (EditText) findViewById(R.id.txtTicketCount);
         txtTicketCode = (EditText) findViewById(R.id.txtTicketCode);
 
+        //Top menu buttons
         btnMenu = (ImageButton) findViewById(R.id.btnMenu);
         btnAbout = (ImageButton) findViewById(R.id.btnAbout);
         btnSettings = (ImageButton) findViewById(R.id.btnSettings);
         btnLogout = (ImageButton) findViewById(R.id.btnLogout);
 
+
+        //Barcode scanner
+        tViewScanner = (DecoratedBarcodeView) findViewById(R.id.tViewScanner);
+        Collection<BarcodeFormat> codeFormats = Arrays.asList(BarcodeFormat.PDF_417, BarcodeFormat.QR_CODE);
+        //tViewScanner.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(codeFormats));
+
+        tViewScanner.decodeContinuous(callback);
+        beepManager = new BeepManager(this);
+
+        //Hide the top menu initially
         menuHide();
 
-
+        //Initialize ValidationActivity button states
         btnAdd.setEnabled(false);
         btnUpload.setEnabled(false);
         btnRemove.setEnabled(false);
 
+        //Listen for Ticket Code field changes
         txtTicketCode.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -205,6 +304,7 @@ public class ValidationActivity extends AppCompatActivity {
             }
         });
 
+        //Listen for Ticket Count field changes
         txtTicketCount.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
